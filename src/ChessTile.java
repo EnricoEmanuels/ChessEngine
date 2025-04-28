4896c822e02ad18bf13f896c78d169efa021d0db
user <enricoemanuels14@gmail.com>
2025-04-28 20:24:18 -0300

    dit is pas het begin

---
+++ src/ChessTile.java
@@ -0,0 +1,15 @@
+public class ChessTile {
+}
+
+/* iedereen weet dat er in een schaakbord 64 hokjes zijn
+* in jouw gedachte zou het lijken op een twee dimentionele array voorbeeld
+* een 8 bij 8
+* maar in deze voorbeeld gaan we een abstractie creeren voor een  eenmalige
+* schaak tile / tegel en we gaan die chess tile / tegel nummeren van o tot 63
+* als je telt vanaf 0 tot 63 heb je in totaal 64 tegels of tiles
+* als je gaat doen van 0 tot 64 dan heb je in totaal 65 tegels en dat is fout
+* vb je wilt 5 tegels dan kan je doen van 0 naar 4 (0, 1, 2, 3, 4) of van
+* 1 naar 5 (1,2,3,4,5)  */
+
+/* een tile is een tegel */
+/* 28-04-2025 ik ben bij de eerste video en ik ben gestopt bij de tijd 2.10 */
